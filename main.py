8681e54aaab1b575fa51061a7c97e33685d32921
Jaspreet Singh <46000676+singhjaspreetb@users.noreply.github.com>
2020-09-29 14:08:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from deepface import DeepFace
+
+from PIL import Image
+
+import cv2
+
+import matplotlib.pyplot as plt
+
+img_path =r'C:\Users\harsh\OneDrive\ONE DRIVE\OneDrive\Desktop\T-Hacks\dataset\img18.jpg'
+
+img = cv2.imread(img_path)
+
+plt.imshow(img[:, :, ::-1])
+
+image = Image.open(img_path)
+image.show()
+
+demography = DeepFace.analyze(img_path)
+
+
+print("Age: ", demography["age"])
+print("Gender: ", demography["gender"])
+print("Emotion: ", demography["dominant_emotion"])
+print("Race: ", demography["dominant_race"])
